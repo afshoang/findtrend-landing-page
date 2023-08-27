e4748c38fa04e784ce4e3bfcf56ccc8ac33c97cc
afshoang <kaitokid19xx@gmail.com>
2023-08-27 12:54:00 +0700

    fixed padding bottom hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import { arrowWhite, tab1, tab2, tab3, tab4 } from '../assets';
 
 const Hero = () => (
-  <div id="home" className="pt-24 h-screen">
+  <div id="home" className="pt-24 pb-[425px] md:pb-[327px]">
     <div className="hero-content w-full text-white mx-auto md:max-w-[728px] text-center max-xs:px-3 relative">
       <h1 data-aos="fade-down" className="text-3xl sm:text-6xl font-extrabold sm:leading-[80px] mb-3">Minimize your tabs.</h1>
       <h1 data-aos="fade-down" data-aos-delay="200" className="text-3xl sm:text-6xl font-extrabold sm:leading-[80px] mb-4">Find the trends!</h1>
@@ -24,28 +24,28 @@ const Hero = () => (
       </div>
     </div>
 
-    <div className="hero-tab max-w-[370px] sm:max-w-[1076px] mx-auto relative mt-14 md:mt-24">
+    <div className="hero-tab max-w-[370px] md:max-w-[1076px] mx-auto relative mt-14 md:mt-24">
       <div
         data-aos="fade-down"
-        className="tab-twitter w-[300px] sm:w-[348px] absolute z-[4] right-[40px] sm:left-0"
+        className="tab-twitter w-[300px] md:w-[348px] absolute z-[4] right-[40px] md:left-0"
       >
         <img src={tab4} alt="tab4" className='-rotate-6' />
       </div>
       <div
         data-aos="fade-down"
-        className="tab-pinterest w-[300px] sm:w-[348px] absolute right-[40px] sm:left-[143px] bottom-[-150px] sm:bottom-[-136px] z-[3]"
+        className="tab-pinterest w-[300px] md:w-[348px] absolute right-[40px] md:left-[143px] bottom-[-150px] md:bottom-[-136px] z-[3]"
       >
         <img src={tab3} alt="tab3" className='-rotate-3' />
       </div>
       <div
         data-aos="fade-down"
-        className="tab-fb w-[300px] sm:w-[348px] absolute right-[40px] sm:left-[390px] top-32 sm:top-0 z-[2]"
+        className="tab-fb w-[300px] md:w-[348px] absolute right-[40px] md:left-[390px] top-32 md:top-0 z-[2]"
       >
         <img src={tab2} alt="tab2" className='rotate-6' />
       </div>
       <div
         data-aos="fade-down"
-        className="tab-dribble w-[300px] sm:w-[348px] absolute z-[1] right-[40px] bottom-[-280px] sm:bottom-[-137px]"
+        className="tab-dribble w-[300px] md:w-[348px] absolute z-[1] right-[40px] bottom-[-280px] md:bottom-[-137px]"
       >
         <img src={tab1} alt="tab1" className='rotate-3' />
       </div>
